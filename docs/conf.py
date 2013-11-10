ab5ebbd285e296591f792b8b7874eef695cc5d21
Ahmet Bakan <lordnapi@gmail.com>
2013-11-10 00:54:24 -0500

    Trying matplotlib extensions on rtfd [skip ci].

--- docs/conf.py
+++ docs/conf.py
@@ -34,8 +34,8 @@ extensions = [
     'sphinx.ext.pngmath',
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
-    'IPython.sphinxext.ipython_console_highlighting',
-    'IPython.sphinxext.ipython_directive']
+    'matplotlib.sphinxext.ipython_console_highlighting',
+    'matplotlib.sphinxext.ipython_directive']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
