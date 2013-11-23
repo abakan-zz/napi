b40554a2fa25524b5d03d8108a51395e68b3b6ee
Ahmet Bakan <lordnapi@gmail.com>
2013-11-23 18:53:31 -0500

    Finished transformer.

--- napi/expression.py
+++ napi/expression.py
@@ -1,15 +1,17 @@
 import ast
 import operator
 
-from ast import Attribute, Call, List, Load, Name, Num, Str, keyword
-from ast import copy_location, fix_missing_locations as fml
+from ast import AST, Attribute, Call, List, Load, Name, Num, Str
+from ast import copy_location as copy, fix_missing_locations as fix
+from ast import iter_fields
 
 import numpy
 from numpy import ndarray
 
 _setdefault = {}.setdefault
 ZERO = lambda dtype: _setdefault(dtype, numpy.zeros(1, dtype)[0])
-
+NONE = Name(id='None', ctx=Load())
+ROOT = (Name(id='False', ctx=Load()), Name(id='True', ctx=Load()))
 OPMAP = {
     'Add': operator.add,
     'Sub': operator.sub,
@@ -33,8 +35,6 @@ OPMAP = {
 
     'Not': operator.not_
 }
-
-END = keyword(arg='end', value=Num(1))
 
 def short_circuit_or(arrays, shape):
 
@@ -79,13 +79,19 @@ class NExpression(object):
 
         self._sc = kwargs.get('sc', kwargs.get('shortcircuit', 0))
         self._sq = kwargs.get('sq', kwargs.get('squeeze', False))
+        self._data = {} # uid: (shape, true, false)
 
-        self._which = self._shape = None
+    def __getitem__(self, uid):
 
+        return self._data.get(uid, (None, None, None))
 
-    def And(self, values, id=None, **kwargs):
+    def __delitem__(self, uid):
 
-        print('@> And(values={}, id={}, **kwargs={})'.format(values, id, kwargs))
+        self._data.pop(uid, None)
+
+    def And(self, values, uid=None, root=False):
+
+        print('@> And(values={}, uid={}, root={})'.format(values, uid, root))
 
         arrays = []
         result = None
@@ -113,23 +119,27 @@ class NExpression(object):
 
         if result is not None:
             if shape:
-                return numpy.zeros(shape, bool)
+                result = numpy.zeros(shape, bool)
             else:
-                return result
+                result = result
         elif arrays:
             if self._sc and numpy.prod(shape) >= self._sc:
-                return short_circuit_and(arrays, shape)
+                result = short_circuit_and(arrays, shape)
             elif len(arrays) == 2:
-                return numpy.logical_and(*arrays)
+                result = numpy.logical_and(*arrays)
             else:
-                return numpy.all(arrays, 0)
+                result = numpy.all(arrays, 0)
         else:
-            return value
+            result = value
+
+        if root:
+            del self[uid]
+        return result
 
 
-    def Or(self, values, id=None, **kwargs):
+    def Or(self, values, uid=None, root=False):
 
-        print('@> Or(values={}, id={}, **kwargs={})'.format(values, id, kwargs))
+        print('@> Or(values={}, uid={}, root={})'.format(values, uid, root))
         arrays = []
         result = None
         shapes = set()
@@ -141,7 +151,7 @@ class NExpression(object):
             elif value:
                 result = value
 
-        if len(shapes) > 1 and kwargs.get('squeeze', kwargs.get('sq', False)):
+        if len(shapes) > 1 and self._sq:
             shapes.clear()
             for i, a in enumerate(arrays):
                 a = arrays[i] = a.squeeze()
@@ -156,27 +166,32 @@ class NExpression(object):
 
         if result is not None:
             if shape:
-                return numpy.ones(shape, bool)
+                result = numpy.ones(shape, bool)
             else:
-                return result
+                result = result
         elif arrays:
             if self._sc and numpy.prod(shape) >= self._sc:
-                return short_circuit_or(arrays, shape)
+                result = short_circuit_or(arrays, shape)
             elif len(arrays) == 2:
-                return numpy.logical_or(*arrays)
+                result = numpy.logical_or(*arrays)
             else:
-                return numpy.any(arrays, 0)
+                result = numpy.any(arrays, 0)
         else:
-            return value
+            result = value
+        if root:
+            del self[uid]
+        return result
 
-    def BinOp(self, left, op, right, id=None):
+    def BinOp(self, left, op, right, uid=None):
 
-        print('@> BinOp(left={}, op={}, right={}, id={})'.format(left, op, right, id))
+        print('@> BinOp(left={}, op={}, right={}, uid={})'
+              .format(left, op, right, uid))
         return OPMAP[op](left, right)
 
-    def Compare(self, left, ops, comparators, id=None):
+    def Compare(self, left, ops, comparators, uid=None):
 
-        print('@> Compare(left={}, ops={}, comparators={}, id={})'.format(left, ops, comparators, id))
+        print('@> Compare(left={}, ops={}, comparators={}, uid={})'
+              .format(left, ops, comparators, uid))
 
         values = []
         for op, right in zip(ops, comparators):
@@ -188,9 +203,10 @@ class NExpression(object):
         else:
             return self.And(values)
 
-    def UnaryOp(self, op, operand, id=None):
+    def UnaryOp(self, op, operand, uid=None):
 
-        print('@> UnaryOp(op={}, operand={})'.format(op, operand))
+        print('@> UnaryOp(op={}, operand={}, uid={})'
+              .format(op, operand, uid))
         return OPMAP[op](operand)
 
 
@@ -200,73 +216,101 @@ class NewTransformer(ast.NodeTransformer):
     def __init__(self, **kwargs):
 
         self._e = kwargs.pop('e', '_E')
-        self._t = False
-        self._i = 0
-        self._id = None
+        self._count = 0
         self._attrs = {}
-        self._kw = []
+        self._id = id(self)
 
     def __getitem__(self, attr):
 
         return self._attrs.setdefault(attr,
-            fml(Attribute(value=fml(Name(id=self._e, ctx=Load())),
+            fix(Attribute(value=Name(id=self._e, ctx=Load()),
                                             attr=attr, ctx=Load())))
 
-    def visit_BoolOp(self, node):
+    @property
+    def _uid(self):
 
-        start = not self._id is not None
+        self._count += 1
+        return Str(s='{}_{}'.format(self._id, self._count))
 
-        if start:
-            self._i += 1
-            self._id = Num(self._i)
-            print('start transforming')
-        self.generic_visit(node)
-        func = self[node.op.__class__.__name__]
-        args = [List(elts=node.values, ctx=Load()), self._id]
-        if start:
-            print('stop transforming')
-            self._id = None
-            kw = self._kw + [END]
+    def visit(self, node, uid=None):
+        """Visit a node."""
+
+        method = 'visit_' + node.__class__.__name__
+        if hasattr(self, method):
+            return getattr(self, method)(node, uid)
         else:
-            kw = self._kw
-        return fml(copy_location(Call(func=func, args=args, keywords=kw), node))
+            return self.generic_visit(node)
 
-    def visit_Compare(self, node):
+    def generic_visit(self, node, uid=None):
+        for field, old_value in iter_fields(node):
+            old_value = getattr(node, field, None)
+            if isinstance(old_value, list):
+                new_values = []
+                for value in old_value:
+                    if isinstance(value, AST):
+                        value = self.visit(value, uid)
+                        if value is None:
+                            continue
+                        elif not isinstance(value, AST):
+                            new_values.extend(value)
+                            continue
+                    new_values.append(value)
+                old_value[:] = new_values
+            elif isinstance(old_value, AST):
+                new_node = self.visit(old_value, uid)
+                if new_node is None:
+                    delattr(node, field)
+                else:
+                    setattr(node, field, new_node)
+        return node
 
-        self.generic_visit(node)
-        if self._id is None:
+    __call__ = visit
+
+    def visit_BoolOp(self, node, uid=None):
+
+        print('~ BoolOp')
+        if uid is None:
+            root = ROOT[1]
+            uid = self._uid
+        else:
+            root = ROOT[0]
+        self.generic_visit(node, uid)
+        func = self[node.op.__class__.__name__]
+        args = [List(elts=node.values, ctx=Load()), uid, root]
+        return fix(copy(Call(func=func, args=args, keywords=[]), node))
+
+    def visit_Compare(self, node, uid=NONE):
+
+        self.generic_visit(node, uid)
+        if uid is None:
             return node
         else:
             print('~ Compare')
             func = self['Compare']
             args = [node.left,
                     List(elts=[Str(op.__class__.__name__) for op in node.ops], ctx=Load()),
-                    List(elts=node.comparators, ctx=Load()), self._id]
-            return copy_location(Call(func=func, args=args, keywords=self._kw), node)
+                    List(elts=node.comparators, ctx=Load()), uid]
+            return copy(Call(func=func, args=args, keywords=[]), node)
 
-    def visit_BinOp(self, node):
+    def visit_BinOp(self, node, uid=NONE):
 
-        self.generic_visit(node)
-        if self._id is None:
+        self.generic_visit(node, uid)
+        if uid is None:
             return node
         else:
             print('~ BinOp')
             func = self['BinOp']
-            args = [node.left, Str(node.op.__class__.__name__), node.right, self._id]
-            return copy_location(Call(func=func, args=args, keywords=self._kw), node)
+            args = [node.left, Str(node.op.__class__.__name__),
+                    node.right, uid]
+            return copy(Call(func=func, args=args, keywords=[]), node)
 
-    def visit_UnaryOp(self, node):
+    def visit_UnaryOp(self, node, uid=NONE):
 
-        self.generic_visit(node)
-        if self._id is None:
+        self.generic_visit(node, uid)
+        if uid is None:
             return node
         else:
             print('~ UnaryOp')
             func = self['UnaryOp']
-            args = [Str(node.op.__class__.__name__), node.operand, self._id]
-            return copy_location(Call(func=func, args=args, keywords=self._kw), node)
-
-
-if __name__ == '__main__':
-
-    reload(exp); _E = exp.NExpression(); t = exp.NewTransformer(); eval(compile(t.visit(ast.parse('[] or not 1 and 2 + 0 < 3', '', 'eval')), '', 'eval'))
+            args = [Str(node.op.__class__.__name__), node.operand, uid]
+            return copy(Call(func=func, args=args, keywords=[]), node)
